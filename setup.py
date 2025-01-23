c04d843cb257dc60b2e8020896ff362d621194e8
Harsh Kumar <harshks2320@gmail.com>
2025-01-23 08:18:49 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harsh Kumar",
+    author_email="harshkumarsingh2320@gmail.com",
+    packages=find_packages()
+)
